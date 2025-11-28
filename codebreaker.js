7a92ae13bb5dd14b2efb1419496ee99afcaa68ae
NightByNight <32313280+NightByNight@users.noreply.github.com>
2025-11-28 10:39:43 +0100

    Add files via upload

--- codebreaker.js
+++ codebreaker.js
@@ -1,19 +1,20 @@
-const textfield = document.getElementById('sse-envents');
+const textfield = document.getElementById('sse-events');
 
-function handleEvent(event){
-    try{
+function handleEvent(event) {
+    try {
         const json = JSON.parse(event.data);
         console.log("Receive event:", json);
-        if(json && typeof json.data == 'string'){
-            textfield.textContent += json.data;
-            textfield.scrollTop = testField.scrollHeight;
-        }
-    } catch(e) {
 
+        if (json && typeof json.data === 'string') {
+            textfield.textContent += json.data;
+            textfield.scrollTop = textfield.scrollHeight;  // Correction : textfield et pas testField
+        }
+    } catch (e) {
+        console.error("Invalid JSON:", event.data);
     }
 }
 
-for(let channel = 1; channel <= 100; channel ++){
-    const es = new EventSource("https://challs.secuia.fr/stream?channel=codebreaker_${channel}");
+for (let channel = 1; channel <= 100; channel++) {
+    const es = new EventSource(`https://challs.secuia.fr/stream?channel=codebreaker_${channel}`);
     es.addEventListener('message', handleEvent);
 }
